b3843fbde7b63aea5febc2bd3d929fbac30dafe8
Stark16 <pradyumnp508@gmail.com>
2020-04-18 19:24:49 +0530

    This script does Handwritten Text recognition

--- HTR.py
+++ HTR.py
@@ -2,3 +2,4 @@ import PDF_to_JPEG
 import cv2
 import numpy
 PDF_to_JPEG.cvt_to_pdf()
+
--- PDF_to_JPEG.py
+++ PDF_to_JPEG.py
@@ -6,11 +6,12 @@ def cvt_to_pdf():
     filename = path.split('/')[-1]
     print("./PDFs/Output/" + filename[:-4])
     try:
-        os.mkdir("./PDFs/Output/" + filename[:-4])
+        output_path = "./PDFs/Output/" + filename[:-4]
+        os.mkdir(output_path)
         pages = convert_from_path(path, 500)
         i = 1
         for page in pages:
-            page_name = filename[:-4] + "_" + str(i) + ".jpg"
+            page_name = output_path + filename[:-4] + "_" + str(i) + ".jpg"
             page.save(page_name, "JPEG")
             i += 1
     except:
